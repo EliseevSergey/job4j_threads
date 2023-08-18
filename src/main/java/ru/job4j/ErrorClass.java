26ec3751cb8adfc86e2145b7f893d5de2377201b
EliseevSergey <s.eliseev1@gmail.com>
2023-08-18 23:55:08 +0300

    OK class

--- src/main/java/ru/job4j/ErrorClass.java
+++ src/main/java/ru/job4j/ErrorClass.java
@@ -2,6 +2,7 @@ package ru.job4j;
 
 public class ErrorClass {
     public static void main(String[] args) {
-        System.out.println("Absolutely correct class");
+
+        System.out.println("OK");
     }
 }
