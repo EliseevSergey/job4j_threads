03a5233b06e6b82262c4cc1e0e2e289159807f8a
EliseevSergey <s.eliseev1@gmail.com>
2023-08-19 12:41:43 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,32 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {
+                }
+        );
+        Thread second = new Thread(
+                () -> {
+                }
+        );
+        System.out.println("Initial status:");
+        System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+        System.out.printf("Thread name: %s, thread status: %s %n %n", second.getName(), second.getState());
+
+        first.start();
+        second.start();
+
+        while ((first.getState() != Thread.State.TERMINATED) || (second.getState() != Thread.State.TERMINATED)) {
+            System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+            System.out.printf("Thread name: %s, thread status: %s %n", second.getName(), second.getState());
+        }
+
+        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
+            System.out.println("Both status before close:");
+            System.out.printf("Thread name: %s, thread status: %s %n", first.getName(), first.getState());
+            System.out.printf("Thread name: %s, thread status: %s %n", second.getName(), second.getState());
+            System.out.println("Main is allowed to say Bye Bye");
+        }
+    }
+}
