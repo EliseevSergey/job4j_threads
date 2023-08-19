b1e96ba50acc14fd99aef72572e73a7a495fe569
EliseevSergey <s.eliseev1@gmail.com>
2023-08-19 18:56:27 +0300

    3.4. Временное хранение. git stash. [#504832]

---
+++ src/main/java/ru/job4j/concurrent/ClassWhileStashed.java
@@ -0,0 +1,7 @@
+package ru.job4j.concurrent;
+
+public class ClassWhileStashed {
+    public static void main(String[] args) {
+        System.out.println("Made while stashed was applied");
+    }
+}
