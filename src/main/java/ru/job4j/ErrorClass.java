ed76c6eef15983bd930dff9063a48d2196eb3a18
EliseevSergey <s.eliseev1@gmail.com>
2023-08-20 14:41:34 +0300

    upd

--- src/main/java/ru/job4j/ErrorClass.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j;
-
-public class ErrorClass {
-    public static void main(String[] args) {
-        System.out.println("RELEASE 1 NO ERROR");
-    }
-}
--- src/main/java/ru/job4j/concurrent/Stash.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.concurrent;
-
-public class Stash {
-    public static void main(String[] args) {
-        System.out.println("Perfect stash");
-    }
-}
