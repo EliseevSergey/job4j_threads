8dd7b7eb7d1fb34b4093c28ffc3e40d32bb5bd45
EliseevSergey <s.eliseev1@gmail.com>
2023-08-21 21:38:37 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,72 +1,84 @@
 package ru.job4j.concurrent;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String path;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String path) {
         this.url = url;
         this.speed = speed;
+        this.path = path;
     }
 
     @Override
     public void run() {
         var startAt = System.currentTimeMillis();
-        var file = new File("./data/tmp2.xml");
+        var file = new File(path);
         try (var in = new URL(url).openStream();
              var out = new FileOutputStream(file)) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
-            var dataBuffer = new byte[512];
-            int bytesRead;
-            while ((bytesRead = in.read(dataBuffer, 0, dataBuffer.length)) != -1) {
-                var downloadAt = System.nanoTime();
-                out.write(dataBuffer, 0, bytesRead);
-                long actualTime = System.nanoTime() - downloadAt;
-                long sleepTime = (1000000 * 512 / actualTime) / speed;
-                    if (sleepTime > 1) {
+            int bytesTotal = 0;
+            int b;
+            var downloadAt = System.nanoTime();
+
+            while ((b = in.read()) != -1) {
+                bytesTotal++;
+                out.write((byte) b);
+                if (bytesTotal == speed) {
+                    long actualTime = (System.nanoTime() - downloadAt) / 1000000;
+                    System.out.printf("Read %s bytes. Time to download %s mls %n", bytesTotal, actualTime);
+                    bytesTotal = 0;
+                    if (actualTime < 1000) {
+                        long sleepTime = 1000 - actualTime;
                         try {
-                            System.out.printf("Sleep was applied : %s mls %n", sleepTime);
                             Thread.sleep(sleepTime);
+                            System.out.printf("Thread.sleep was applied: %s mls %n", sleepTime);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
+                        downloadAt = System.nanoTime();
                     }
-                System.out.printf("Read 512 bytes : %s  . nano: SleepTime: %s mls %n", actualTime, sleepTime);
+                }
             }
-        } catch (IOException e) {
+            System.out.printf("Total file download size: %s byte", Files.size(file.toPath()));
+        } catch (MalformedURLException e) {
             e.printStackTrace();
-        }
-        try {
-            System.out.printf("bytes: %s", Files.size(file.toPath()));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static void validation(String[] args) {
-        if (args.length != 2) {
+        public static void validation(String[] args) {
+        if (args.length != 3) {
             throw new IllegalArgumentException("Not enough arguments");
         }
-        String url = args[0];
         UrlValidator urlValidator = new UrlValidator();
-        urlValidator.validateUrl(url);
+        urlValidator.validateUrl(args[0]);
         int speed = Integer.parseInt(args[1]);
-        if (speed <= 0) {
+        if (Integer.parseInt(args[1]) <= 0) {
             throw new IllegalArgumentException(String.format("Speed is wrong %s ", speed));
-        }
+            }
+        Path path = Path.of(args[2]);
+            if (!path.toFile().exists()) {
+                throw new IllegalArgumentException(String.format("File [%s] not exist ", path));
+            }
     }
 
     public static void main(String[] args) throws InterruptedException {
         validation(args);
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String path = args[2];
+        Thread wget = new Thread(new Wget(url, speed, path));
         wget.start();
         wget.join();
     }
