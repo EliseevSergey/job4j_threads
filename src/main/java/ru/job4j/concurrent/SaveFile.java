2a9bb1a1aa97b412608b5c71bd9becee5388e305
EliseevSergey <s.eliseev1@gmail.com>
2023-08-29 23:19:24 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/SaveFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.concurrent;
+
+import java.io.*;
+
+public final class SaveFile {
+    private final File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized void saveContent(String content) {
+        try (FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw)) {
+                bw.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
