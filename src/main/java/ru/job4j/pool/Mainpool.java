e432b1c27238eec9e0c462a7c69fe3fb4e0e483d
EliseevSergey <s.eliseev1@gmail.com>
2023-11-20 21:52:01 +0300

    upd

--- src/main/java/ru/job4j/pool/Mainpool.java
+++ src/main/java/ru/job4j/pool/Mainpool.java
@@ -2,14 +2,18 @@ package ru.job4j.pool;
 
 public class Mainpool {
     public static void main(String[] args) {
-        ThreadPool threadPool = new ThreadPool(3);
+        ThreadPool threadPool = new ThreadPool();
         for (int i = 0; i < 10; i++) {
             int taskNo = i;
             Thread thread = new Thread(() -> {
                 System.out.printf("Thread name: %s. Task Number: %s %n",
                         Thread.currentThread().getName(), taskNo);
             });
-            threadPool.work(thread);
+            try {
+                threadPool.work(thread);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
