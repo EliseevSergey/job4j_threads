09f54243db9f9dfad41c11ad71928399cf9f1315
Sergey Eliseev <s.eliseev1@gmail.com>
2023-11-23 00:05:45 +0300

    2. ExecutorService рассылка почты. [#63097 #461630]

--- src/main/java/ru/job4j/concurrent/email/EmailNotification.java
+++ src/main/java/ru/job4j/concurrent/email/EmailNotification.java
@@ -4,13 +4,9 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    private boolean isStopped = false;
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public synchronized void emailTo(User user) {
-        if (isStopped) {
-            throw new IllegalStateException("Pool is stopped");
-        }
         String subject = String.format("Notification %s to email %s", user.getName(), user.getEmail());
         String body = String.format("Add a new event to %s", user.getName());
         pool.submit(() -> {
@@ -19,15 +15,16 @@ public class EmailNotification {
     }
 
     public synchronized void send(String subject, String body, String email) {
-        if (isStopped) {
-            throw new IllegalStateException("Pool is stopped");
-        }
     }
 
     public synchronized void close() {
-        isStopped = true;
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         pool.shutdown();
     }
-
-
 }
