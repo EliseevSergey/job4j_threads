93dad5d5c63703d353174a02533792d9d9ece7ec
Sergey Eliseev <s.eliseev1@gmail.com>
2023-12-16 22:37:58 +0300

    demo

---
+++ src/main/java/ru/job4j/concurrent/completablefuture/ComplDemo.java
@@ -0,0 +1,82 @@
+package ru.job4j.concurrent.completablefuture;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class ComplDemo {
+    private static void iWork() throws InterruptedException {
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Вы: Я работаю");
+            TimeUnit.SECONDS.sleep(1);
+            count++;
+        }
+    }
+
+
+    public static CompletableFuture<Void> goToTrash() {
+        return CompletableFuture.runAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел выносить мусор");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я вернулся!");
+                }
+        );
+    }
+
+    public static CompletableFuture<String> buyProduct(String product) {
+        return CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Сын: Мам/Пам, я пошел в магазин");
+                    try {
+                        TimeUnit.SECONDS.sleep(5);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("Сын: Мам/Пап, я купил " + product);
+                    return product;
+                }
+        );
+    }
+
+    public static CompletableFuture<Void> washHands(String name) {
+        return CompletableFuture.runAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(name + ", моет руки");
+        });
+    }
+
+    public static CompletableFuture<String> whoWashHands(String name) {
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return name + ", моет руки";
+        });
+    }
+
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        CompletableFuture<Object> first = CompletableFuture.anyOf(
+                whoWashHands("Папа"), whoWashHands("Мама"),
+                whoWashHands("Ваня"), whoWashHands("Боря")
+        );
+
+        CompletableFuture<Void> all = CompletableFuture.allOf(washHands("Batya"), washHands("Matya"), washHands("Vanya"), washHands("Borya"));
+        System.out.println("Кто сейчас моет руки?");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println(first.get());
+        System.out.println(all.get());
+    }
+}
---
+++ src/main/java/ru/job4j/pool/TwoDim.java
@@ -0,0 +1,38 @@
+package ru.job4j.pool;
+
+public class TwoDim {
+    private static int sum(int[][] in) {
+        int rowSum = 0;
+        for (int row = 0; row < in.length; row++) {
+            for (int col = 0; col < in[row].length; col++) {
+                rowSum = rowSum + in[row][col];
+            }
+        }
+            return rowSum;
+    }
+
+    private static int sumVert(int[][] in) {
+        int rowSize = in.length;
+        int colSize = in[0].length;
+
+        int colSum = 0;
+        for (int col = 0; col < colSize; col++) {
+            for (int row = 0; row < rowSize; row++) {
+                colSum = colSum + in[row][col];
+            }
+        }
+        return colSum;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] array2 = {{5, 7, 3, 17}, {1, 2, 4, 5}, {6, 7, 8, 9}};
+        int[][] array3 = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+        System.out.println(array2.length);
+        System.out.println(array2[0][3]);
+        System.out.println(array2[0].length);
+        System.out.println(sum(array3));
+        System.out.println(sumVert(array3));
+    }
+
+}
