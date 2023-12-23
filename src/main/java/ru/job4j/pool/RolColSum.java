edec0170c4f65086df692a530514762abba79800
Sergey Eliseev <s.eliseev1@gmail.com>
2023-12-23 12:55:00 +0300

    4. CompletableFuture [#361626 #466940]

--- src/main/java/ru/job4j/pool/RolColSum.java
+++ src/main/java/ru/job4j/pool/RolColSum.java
@@ -1,5 +1,8 @@
 package ru.job4j.pool;
 
+import ru.job4j.concurrent.completablefuture.Sums;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,78 +10,34 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 public class RolColSum {
-    public static class Sums {
-        private int rowSum;
-        private int colSum;
-
-        public int getRowSum() {
-            return rowSum;
-        }
-
-        public int getColSum() {
-            return colSum;
-        }
-
-        public void setRowSum(int rowSum) {
-            this.rowSum = rowSum;
-        }
-
-        public void setColSum(int colSum) {
-            this.colSum = colSum;
-        }
-
-        @Override
-        public String toString() {
-            return "Sums{"
-                    + "rowSum=" + rowSum
-                    + ", colSum=" + colSum
-                    + '}';
-        }
-    }
-
     public static Sums[] sum(int[][] matrix) {
-        int rowSize = matrix.length;
-        int colSize = matrix[0].length;
-        int rslSize = Math.max(rowSize, colSize);
-        Sums[] rsl = new Sums[rslSize];
-        for (int i = 0; i < rslSize; i++) {
-            rsl[i] = new Sums();
-        }
-        int rowSum = 0;
-        for (int row = 0; row < rowSize; row++) {
-            for (int col = 0; col < colSize; col++) {
-                rowSum += matrix[row][col];
+        ArrayList<Sums> rsl = new ArrayList<>();
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if (rsl.size() <= row || rsl.size() <= col) {
+                    rsl.add(new Sums());
+                }
+                int val = matrix[row][col];
+                int curRow = rsl.get(row).getRowSum();
+                int curCol = rsl.get(col).getColSum();
+                rsl.get(row).setRowSum(curRow + val);
+                rsl.get(col).setColSum(curCol + val);
             }
-            rsl[row].setRowSum(rowSum);
-            rowSum = 0;
         }
-        int colSum = 0;
-        for (int col = 0; col < colSize; col++) {
-            for (int row = 0; row < rowSize; row++) {
-                colSum += matrix[row][col];
-            }
-            rsl[col].setColSum(colSum);
-            colSum = 0;
-        }
-        return rsl;
+        return rsl.toArray(Sums[]::new);
     }
 
     public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
-        int rowSize = matrix.length;
-        int colSize = matrix[0].length;
-        int rslSize = Math.max(rowSize, colSize);
-        Sums[] rsl = new Sums[rslSize];
+        int rslSize = Math.max(matrix.length, matrix[0].length);
+        ArrayList<Sums> rsl = new ArrayList<>();
         Map<Integer, CompletableFuture<Sums>> futures = new HashMap<>();
-        for (int i = 0; i < rslSize / 2; i++) {
-            futures.put(i, getRowColSum(matrix, i, i));
-        }
-        for (int i = rslSize / 2; i < rslSize; i++) {
+        for (int i = 0; i < rslSize; i++) {
             futures.put(i, getRowColSum(matrix, i, i));
         }
         for (Integer key : futures.keySet()) {
-            rsl[key] = futures.get(key).get();
+            rsl.add(futures.get(key).get());
         }
-        return rsl;
+        return rsl.toArray(Sums[]::new);
     }
 
     public static CompletableFuture<Sums> getRowColSum(int[][] data, int row, int column) {
@@ -94,8 +53,6 @@ public class RolColSum {
                         for (int r = 0; r < data.length; r++) {
                             colSum += data[r][column];
                         }
-                        cell.setColSum(colSum);
-                    } else {
                         cell.setColSum(colSum);
                     }
                     return cell;
--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -1,6 +1,7 @@
 package ru.job4j.pool;
 
 import org.junit.jupiter.api.Test;
+import ru.job4j.concurrent.completablefuture.Sums;
 
 import java.util.concurrent.ExecutionException;
 
@@ -10,23 +11,24 @@ class RolColSumTest {
     @Test
     public void whenOk() {
         int[][] in = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
-        RolColSum.Sums[] rsl = RolColSum.sum(in);
-        for (RolColSum.Sums cell : rsl) {
-            System.out.println(cell);
-        }
-        assertEquals(3, rsl[1].getColSum());
-        assertEquals(4, rsl[2].getRowSum());
+        Sums[] rsl = RolColSum.sum(in);
+        Sums s0 = new Sums(4, 3);
+        Sums s1 = new Sums(4, 3);
+        Sums s2 = new Sums(4, 3);
+        Sums s3 = new Sums(0, 3);
+        Sums[] exp = {s0, s1, s2, s3};
+        assertArrayEquals(exp, rsl);
     }
 
     @Test
     public void whenSmall() throws ExecutionException, InterruptedException {
-        int[][] in = {{1, 1, 1}, {2, 3, 4}, {5, 6, 7}};
-        RolColSum.Sums[] rsl = RolColSum.asyncSum(in);
-        assertEquals(3, rsl[0].getRowSum());
-        assertEquals(9, rsl[1].getRowSum());
-        assertEquals(18, rsl[2].getRowSum());
-        assertEquals(8, rsl[0].getColSum());
-        assertEquals(10, rsl[1].getColSum());
-        assertEquals(12, rsl[2].getColSum());
+        int[][] in = {{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}};
+        Sums s0 = new Sums(4, 3);
+        Sums s1 = new Sums(4, 3);
+        Sums s2 = new Sums(4, 3);
+        Sums s3 = new Sums(0, 3);
+        Sums[] rsl = RolColSum.asyncSum(in);
+        Sums[] exp = {s0, s1, s2, s3};
+        assertArrayEquals(exp, rsl);
     }
 }
